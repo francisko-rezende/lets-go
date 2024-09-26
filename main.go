d920aebb21e4fffa3892ac556da8bec6eb5765f8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-09-25 22:06:33 -0300

    feat: finish method-based routing section

--- main.go
+++ main.go
@@ -25,14 +25,19 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("create a new snippet"))
+	w.Write([]byte("Display a form for creating a snippet"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Save a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("starting server on :4000")
 	error := http.ListenAndServe(":4000", mux)
