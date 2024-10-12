ac58aea4c401848b9f31de5f744005fc11c323a8
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-10-12 18:37:10 -0300

    feat: finish html templating and inheritance section

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from snippetbox"))
+	// path is relative to current working directory
+	files := []string{
+		"./ui/html/pages/home.tmpl.html",
+		"./ui/html/base.tmpl.html",
+		"./ui/html/nav.tmpl.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(w, "Internal server error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
