800e5b2aae3e46a2f3d8066d9820287e3f519fa5
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-11-02 11:51:14 -0300

    feat: finish creating a connection pool section

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // New import
 )
 
 type config struct {
@@ -19,16 +22,40 @@ func main() {
 	var cfg config
 
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		logger: logger,
 	}
 
 	logger.Info("starting server", slog.String("addr", cfg.addr))
-	error := http.ListenAndServe(cfg.addr, app.routes())
-	logger.Error(error.Error())
+	err = http.ListenAndServe(cfg.addr, app.routes())
+	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
