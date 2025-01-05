3c7889002be5720d635536df3bde1eabe45cf532
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-01-05 10:43:18 -0300

    feat: finish "running a https server" section

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -56,6 +56,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = mysqlstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,
@@ -72,7 +73,7 @@ func main() {
 	}
 
 	logger.Info("starting server", slog.String("addr", srv.Addr))
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 	// maybe replace with
