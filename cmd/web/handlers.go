81946bd3ce4579e899e5986d5aebcfcb99712214
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2025-01-11 18:18:06 -0300

    feat: finish "user signup and password encryption" section

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -52,7 +52,22 @@ func (app *application) userSignUpPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, "Create a new user")
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicatedEmail) {
+			form.AddFieldError("email", "Email already in use")
+
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, r, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+		} else {
+			app.serverError(w, r, err)
+		}
+	}
+
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. Please log in.")
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
--- internal/models/users.go
+++ internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -17,21 +22,28 @@ type UserModel struct {
 	DB *sql.DB
 }
 
-func (u *UserModel) Insert(name, email, password string) (int, error) {
-	stmt := `INSERT into users (name, email, hashedPassword, created)
+func (u *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT into users (name, email, hashed_password, created)
 	VALUES(?, ?, ?, UTC_TIMESTAMP())`
 
-	result, err := u.DB.Exec(stmt, name, email, password)
+	_, err = u.DB.Exec(stmt, name, email, hashedPassword)
 	if err != nil {
-		return 0, err
+		var mySQLErr *mysql.MySQLError
+
+		if errors.As(err, &mySQLErr) {
+			if mySQLErr.Number == 1062 && strings.Contains(mySQLErr.Message, "users_uc_email") {
+				return ErrDuplicatedEmail
+			}
+		}
+		return err
 	}
 
-	id, err := result.LastInsertId()
-	if err != nil {
-		return 0, err
-	}
-
-	return int(id), nil
+	return nil
 }
 
 func (u *UserModel) Authenticate(email, password string) error {
